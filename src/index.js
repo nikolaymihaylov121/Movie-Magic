705a3a728651783ae3aa3e0546af85d682c83215
nikolaymihaylov121 <nikolaymihaylov121@gmail.com>
2025-01-18 16:51:11 +0200

    setting router

---
+++ src/index.js
@@ -0,0 +1,23 @@
+import express from "express";
+import handlebars from "express-handlebars"
+import routes from "./routes.js";
+
+const app = express();
+
+app.engine('hbs', handlebars.engine({
+    extname: 'hbs',
+}))
+app.set('view engine', 'hbs');
+app.set('views', './src/views');
+app.use('/static', express.static('src/public'));
+app.use(routes);
+
+
+
+
+
+
+app.listen(5000, () => {
+    console.log('Server is listening on http://localhost:5000...');
+
+})
