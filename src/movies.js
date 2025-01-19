c0e607d82cbf92b4c463bed986785097abd4a292
nikolaymihaylov121 <nikolaymihaylov121@gmail.com>
2025-01-19 17:23:38 +0200

    add dynamic movie rendering on homePage

---
+++ src/movies.js
@@ -0,0 +1,19 @@
+export default [{
+    title: 'Jungle Cuise',
+    genre: 'Adventure',
+    description: "Based on Disneyland's theme park ride where a small riverboat takes a group of travelers through a jungle filled with dangerous animals and reptiles but with a supernatural element.",
+    imageUrl: '/static/img/jungle-cruise.jpeg'
+},
+{
+    title: 'The Little Mermaid',
+    genre: 'Fantasy',
+    description: "A young mermaid makes a deal with a sea witch to trade her beautiful voice for human legs so she can discover the world above water and impress a prince.",
+    imageUrl: '/static/img/the-little-mermaid.jpg'
+},
+{
+    title: 'Home Alone',
+    genre: 'Comedy',
+    description: "An eight-year-old troublemaker, mistakenly left home alone, must defend his home against a pair of burglars on Christmas Eve.",
+    imageUrl: '/static/img/home-alone.jpeg'
+}
+];
